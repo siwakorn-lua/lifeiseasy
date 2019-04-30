b69dfe727968763029485b23d1d5ddc6c0efbf78
Siwakorn Luangcharoenpong <siwakorn.lua@gmail.com>
2019-04-30 15:18:34 +0700

    done layouting

--- app/src/main/java/com/siwakorn/lifeiseasy/HomeActivity.java
+++ app/src/main/java/com/siwakorn/lifeiseasy/HomeActivity.java
@@ -11,10 +11,6 @@ public class HomeActivity extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //just change the fragment_dashboard
-        //with the fragment you want to inflate
-        //like if the class is HomeFragment it should have R.layout.home_fragment
-        //if it is DashboardFragment it should have R.layout.fragment_dashboard
         return inflater.inflate(R.layout.activity_home, null);
     }
 }
