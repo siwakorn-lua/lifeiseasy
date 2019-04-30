7425c579858e5030b234c4959f082ab84a412d94
Siwakorn Luangcharoenpong <siwakorn.lua@gmail.com>
2019-04-30 12:51:54 +0700

    add layout template

--- app/src/main/java/com/siwakorn/lifeiseasy/LoginActivity.java
+++ app/src/main/java/com/siwakorn/lifeiseasy/LoginActivity.java
@@ -1,13 +1,27 @@
 package com.siwakorn.lifeiseasy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.loginButton);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent home = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(home);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/siwakorn/lifeiseasy/MainActivity.java
@@ -0,0 +1,54 @@
+package com.siwakorn.lifeiseasy;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadFragment(new HomeActivity());
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeActivity();
+                    break;
+                case R.id.navigation_add_job:
+                    fragment = new AddJobActivity();
+                    break;
+                case R.id.navigation_profile:
+                    fragment = new ProfileActivity();
+                    break;
+            }
+            return loadFragment(fragment);
+        }
+    };
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+}
