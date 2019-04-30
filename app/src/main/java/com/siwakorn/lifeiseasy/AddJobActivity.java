995600fcde9c7d51fdae94332b2a52b2f2ccdb61
Siwakorn Luangcharoenpong <siwakorn.lua@gmail.com>
2019-04-30 17:26:25 +0700

    done add job

--- app/src/main/java/com/siwakorn/lifeiseasy/AddJobActivity.java
+++ app/src/main/java/com/siwakorn/lifeiseasy/AddJobActivity.java
@@ -6,11 +6,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddJobActivity extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_add_job, null);
+
+        View view = inflater.inflate(R.layout.activity_add_job, null);
+        Spinner jobSpinner = view.findViewById(R.id.jobSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.jobListWithoutAll, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        jobSpinner.setAdapter(adapter);
+        return view;
+
     }
 }
